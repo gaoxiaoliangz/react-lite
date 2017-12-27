4f2c736e4067a27487a0242fe7cdd5ce09d91554
Liang <gaoxiaoliangz@users.noreply.github.com>
2017-12-27 22:02:53 +0800

    react ok

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => {
+  return <div>
+    Yes!
+  </div>
+}
+
+ReactDom.render(<App/>, document.getElementById('root'))
