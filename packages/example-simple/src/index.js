2b919333c7d822129bf457243b797e0503d7dc21
Liang <gaoxiaoliangz@gmail.com>
2018-10-31 12:58:24 +0800

    mv files

--- packages/example-simple/src/index.js
+++ packages/example-simple/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import MyReact, { render as myRender } from 'my-react'
+import React from 'react'
+import { render } from 'react-dom'
+import test from './test'
+import './style.css'
+// import test0 from './test-simple'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = test(MyReact, React.Component, true)
+const App2 = test(React, React.Component, true)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'))
+render(<App2 />, document.getElementById('root2'))
